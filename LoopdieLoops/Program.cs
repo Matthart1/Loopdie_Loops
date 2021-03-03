fdf113d9e66157075e2c530ca0d1c3b81641ccc4
Matt Hart <mp.hart047@gamil.com>
2021-03-02 20:05:54 -0600

    Loopies v1.0

---
+++ LoopdieLoops/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace LoopdieLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"[i]");
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"[i]");
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"[i]");
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"[i]");
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"[i]");
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"[i]");
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"[i]");
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"[i]");
+            }
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"[i]");
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"[i]");
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"[i]");
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"[i]");
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"[i]");
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"[i]");
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"[i]");
+            }
+        }
+    }
+}
